758fd011efb7b150cc5aba4e72d6f8db53ec914c
AbuSamrah <abdullah@kiitos-tech.com>
2021-02-23 16:03:32 +0200

    first commit

--- view/index.js
+++ view/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+const cors  = require('cors');
 const pokemonRouter = require("../routes/pokemon")
 const app = express();
 
 let port = process.env.PORT || 3000;
-
+app.use(cors()); // <---- use cors middleware
 app.get("/", (req, res) => {
     res.send("Welcome to Pokemons API, use the following links  1- https://samrah-api-poke.herokuapp.com/pokemon/search/PokemonName : to search for a pokemmon 2- https://samrah-api-poke.herokuapp.com/pokemon/PokemonID to get a pokemon by its id")
 });
