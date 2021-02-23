b528e3ba4fac9b587a5fe91ee5ffc84c71f3051f
AbuSamrah <abdullah@kiitos-tech.com>
2021-02-23 15:42:10 +0200

    first commit

--- view/index.js
+++ view/index.js
@@ -6,7 +6,7 @@ const app = express();
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("holas")
+    res.send("Welcome to Pokemons API, use the following links  1- https://samrah-api-poke.herokuapp.com/pokemon/search/PokemonName : to search for a pokemmon 2- https://samrah-api-poke.herokuapp.com/pokemon/PokemonID to get a pokemon by its id")
 });
 
 app.use("/pokemon", pokemonRouter);
